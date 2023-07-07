2ad44816bc4176a60290484c0c05970c4df730ec
Badrul <badrul.alam@bjitgroup.com>
2023-07-07 11:20:59 +0600

    Initial commit

---
+++ src/main/java/com/badrul/ecommercepoc/EcommercePocApplication.java
@@ -0,0 +1,13 @@
+package com.badrul.ecommercepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePocApplication.class, args);
+	}
+
+}
